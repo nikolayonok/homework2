63531767e5d7f1b327a355a2074cbf2dcc353c61
makbuk <141294>
2021-05-21 12:14:35 +0300

    task11

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите день недели ");
+        int a = sc.nextByte();
+        if (a <= 0 || a > 7 ) {
+            System.out.println("Mistake");
+        }
+        switch (a) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
 
     }
 }
