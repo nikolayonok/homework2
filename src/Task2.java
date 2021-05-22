3799c2e88e43fe706cdb4b40656940626e041a90
makbuk <141294>
2021-05-22 14:42:05 +0300

    task2

--- src/Task2.java
+++ src/Task2.java
@@ -5,7 +5,14 @@ public class Task2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите время ");
         int a = sc.nextInt();
-
-        
+        if (a >= 1 && a <= 8){
+            System.out.println("Good Morning");
+        } else if (a >=9 && a <= 17){
+            System.out.println("Good Day");
+        } else if (a >=18 && a <= 21){
+            System.out.println("Good evening");
+        } else if (a >=22 && a <= 24){
+            System.out.println("Good Night");
+        } else System.out.println("Mistake");
     }
 }
